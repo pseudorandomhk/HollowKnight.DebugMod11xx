c5c854352a18d575f71963995a3d9879e52fc275
pseudorandom <7276677+pseudorandomhk@users.noreply.github.com>
2022-10-15 15:04:25 -0400

    Change default keybindings to match Minidebug's

--- DebugMod11xx/Settings.cs
+++ DebugMod11xx/Settings.cs
@@ -6,50 +6,27 @@ namespace DebugMod;
 [Serializable]
 public class Settings
 {
-    public string toggleInfo;
-    public string toggleInfiniteSoul;
-    public string toggleInfiniteHealth;
-    public string toggleInvincibility;
-    public string toggleVignette;
-    public string toggleNoclip;
-    public string cameraFollow;
-    public string cycleHitboxes;
+    public string toggleInfo = "f1";
+    public string toggleInfiniteSoul = "f3";
+    public string toggleInfiniteHealth = "f2";
+    public string toggleInvincibility = "f4";
+    public string toggleVignette = "insert";
+    public string toggleNoclip = "[0]";
+    public string cameraFollow = "f8";
+    public string cycleHitboxes = "[5]";
 
-    public string increaseLoadExtension;
-    public string decreaseLoadExtension;
+    public string increaseLoadExtension = "f5";
+    public string decreaseLoadExtension = "f6";
 
-    public string zoomIn;
-    public string zoomOut;
-    public string resetZoom;
+    public string zoomIn = "page up";
+    public string zoomOut = "page down";
+    public string resetZoom = "home";
 
-    public string increaseTimescale;
-    public string decreaseTimescale;
-    public string resetTimescale;
+    public string increaseTimescale = "[";
+    public string decreaseTimescale = "]";
+    public string resetTimescale = "\\";
 
-    public string saveSavestate;
-    public string loadSavestate;
-    public string loadSavestateDuped;
-
-    public Settings()
-    {
-        toggleInfo = "f4";
-        toggleInfiniteSoul = "b";
-        toggleInfiniteHealth = "n";
-        toggleInvincibility = "m";
-        toggleVignette = "insert";
-        toggleNoclip = "left shift";
-        cameraFollow = "f8";
-        cycleHitboxes = "h";
-        increaseLoadExtension = "=";
-        decreaseLoadExtension = "-";
-        zoomIn = "page up";
-        zoomOut = "page down";
-        resetZoom = "home";
-        increaseTimescale = ".";
-        decreaseTimescale = ",";
-        resetTimescale = "/";
-        saveSavestate = "f1";
-        loadSavestate = "f2";
-        loadSavestateDuped = "f3";
-    }
+    public string saveSavestate = "[7]";
+    public string loadSavestate = "[8]";
+    public string loadSavestateDuped = "[9]";
 }
